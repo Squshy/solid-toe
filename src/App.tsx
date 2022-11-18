da7a1e230431906564e90c59c73de7a374c34dc8
Calvin Lapp <lapp.cd@gmail.com>
2022-11-17 20:38:14 -0500

    Init solid project with tailwind

---
+++ src/App.tsx
@@ -0,0 +1,7 @@
+import type { Component } from "solid-js";
+
+const App: Component = () => {
+  return <div class="bg-red-500">Hey</div>;
+};
+
+export default App;
---
+++ tailwind.config.js
@@ -0,0 +1,9 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: ["./src/**/*.tsx", "./index.html"],
+  darkMode: "class",
+  theme: {
+    extend: {},
+  },
+  plugins: [],
+};
