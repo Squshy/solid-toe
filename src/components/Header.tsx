df6850ab8ad737f36a4b5df34f4b2f1e1cf49b00
Calvin Lapp <lapp.cd@gmail.com>
2022-11-22 18:41:06 -0500

    Rename to togglethemebutton

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -1,11 +1,11 @@
 import type { Component } from "solid-js";
-import ThemeToggle from "./ThemeToggle";
+import ToggleThemeButton from "./ToggleThemeButton";
 
 const Header: Component = () => {
   return (
     <div class="w-full p-4 flex flex-row">
       <div>Solid Toe</div>
-      <ThemeToggle />
+      <ToggleThemeButton />
     </div>
   );
 };
